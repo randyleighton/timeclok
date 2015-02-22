1e9717d4be4c9388b67160f2356ae4340e63e8eb
Randy Leighton <randyleighton@yahoo.com>
2015-02-21 20:11:46 -0800

    update timecard controller and add home route

--- app/controllers/timecards_controller.rb
+++ app/controllers/timecards_controller.rb
@@ -1,12 +1,20 @@
 class TimecardsController < ApplicationController
-
+  before_filter :find_timecard, except: [:index,:new,:create]
   def index
+
   end
 
   def new
+    @timecard = Timecard.new
   end
 
   def create
+    @timecard = Timecard.create(timecard_params)
+    if @timecard.valid?
+      redirect_to timecard_path(@timecard)
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -19,6 +27,16 @@ class TimecardsController < ApplicationController
   end
 
   def destroy
+  end
+
+private
+  
+  def timecard_params
+    params.require(:timecard).permit(:start_time, :stop_time, :lunch_start, :lunch_stop)
+  end
+
+  def find_timecard
+    @timecard = Timecard.find(params[:id])
   end
 
 end
