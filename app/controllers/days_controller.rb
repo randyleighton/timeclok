c465e28ad51081c2fb41915e25e668eb899252e7
Randy Leighton <randyleighton@yahoo.com>
2015-02-26 18:56:34 -0800

    start days controller

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,25 @@
+class DaysController < ApplicationController
+
+  def index
+    @days = Day.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+end
