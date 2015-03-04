16c15533924b9ddb51e7afdad46e1fe0ef6a14dc
Randy Leighton <randyleighton@yahoo.com>
2015-03-03 19:13:56 -0800

    fix nested relation

--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -2,7 +2,7 @@ class Timecard < ActiveRecord::Base
 
   has_many :days, dependent: :destroy
 
-  accepts_nested_attributes_for :day, :reject_if => :all_blank
+  accepts_nested_attributes_for :days, :reject_if => :all_blank
 
   scope :order_by, ->{ joins(:day).order('day.start_time ASC') }
 
