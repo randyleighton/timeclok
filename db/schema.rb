76231e4ff1ecd8384d9a57b905f154dfc070497f
Randy Leighton <randyleighton@yahoo.com>
2015-03-05 07:59:03 -0800

    start user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226032422) do
+ActiveRecord::Schema.define(version: 20150305155748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,19 @@ ActiveRecord::Schema.define(version: 20150226032422) do
   create_table "timecards", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "password_digest"
+    t.boolean  "email_confirmed"
+    t.string   "remember_token"
+    t.string   "password_reset_token"
+    t.datetime "password_sent_at"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
 end
