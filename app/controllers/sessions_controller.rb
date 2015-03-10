5db9f504671e243c27377678103ccc69c9620c57
Randy Leighton <randyleighton@yahoo.com>
2015-03-09 19:00:38 -0700

    sessions controller and mnethods to create the sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.authenticate_user(params[:email], params[:password])
+    if user
+      session[:user] = user.id
+      redirect_to root_url
+    else
+      flash.now.alert = "Invalid email or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user].destroy
+    redirect_to root_url, notice: "Signing Out Successfully"
+  end
+
+end
