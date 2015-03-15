bc243c230326d7e406d18f8902741d13af197ae1
Randy Leighton <randyleighton@yahoo.com>
2015-03-14 21:46:21 -0700

    add tests

--- spec/models/timecard_spec.rb
+++ spec/models/timecard_spec.rb
@@ -3,4 +3,9 @@ RSpec.describe Timecard, :type => :model do
 it { should have_many :days }
 it { should belong_to :user }
 
+it "should take a start date and stop date for the timecard" do
+  timecard = Timecard.create(start_date:DateTime.now, end_date:DateTime.now + 7)
+  expect (Timecard.all).to eq [timecard]
+end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,9 @@ it { should validate_presence_of :name }
 it { should validate_presence_of :email }
 it { should validate_presence_of :password }
 
+it "should create a user" do
+  user = User.create(name:"tester", email:"tester@test.com", password:"test")
+  expect(User.all).to eq [user]
+end
+
 end
