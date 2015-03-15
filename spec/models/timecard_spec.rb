07f23db23871a62b77137c6eaf4d6cd520d31a7c
Randy Leighton <randyleighton@yahoo.com>
2015-03-15 12:23:05 -0700

    update timecard create test pass

--- spec/models/timecard_spec.rb
+++ spec/models/timecard_spec.rb
@@ -1,11 +1,13 @@
-RSpec.describe Timecard, :type => :model do
+describe Timecard do
 
-it { should have_many :days }
-it { should belong_to :user }
+  it { should have_many :days }
+  it { should belong_to :user }
 
-it "should take a start date and stop date for the timecard" do
-  timecard = Timecard.create(start_date:DateTime.now, end_date:DateTime.now + 7)
-  expect (Timecard.all).to eq [timecard]
-end
+  describe "Timecard Creation" do
+    it "should take a start date and stop date for the timecard" do
+      timecard = Timecard.create(start_date:DateTime.now, end_date:DateTime.now + 7)
+      expect(Timecard.all).to eq [timecard]
+    end
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,13 @@
 RSpec.describe User, :type => :model do
 
-it { should validate_presence_of :name }
-it { should validate_presence_of :email }
-it { should validate_presence_of :password }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
 
-it "should create a user" do
-  user = User.create(name:"tester", email:"tester@test.com", password:"test")
-  expect(User.all).to eq [user]
-end
+  it "should create a user" do
+    user = User.create(name:"tester", email:"tester@test.com", password:"test")
+    binding.pry
+    expect(User.all).to eq [user]
+  end
 
 end
