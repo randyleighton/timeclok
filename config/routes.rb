34cff933ccf92326ce394d159670b5c8d4849fcb
Randy Leighton <randyleighton@yahoo.com>
2015-03-24 06:42:19 -0700

    updating session form and routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ resources :days
 get "/log-in" => "sessions#new"
 post "/log-in" => "sessions#create"
 get "/log-out" => "sessions#destroy", as: :log_out
-# resources :sessions, only: [:new, :create, :destroy]
+resources :sessions, only: [:new, :create, :destroy]
 
 end
