0732d4e9bf7ff95906e9a9ae73a52c7a4d3e820d
Randy Leighton <randyleighton@yahoo.com>
2015-03-27 18:02:03 -0700

    fix user save to work with errors layout in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       redirect_to root_url, notice: "Created Account for #{@user.name}"
     else
       render 'new'
