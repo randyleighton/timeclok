ab0c19af4869f519ae35c35173f9061e46f2da5b
Randy Leighton <randyleighton@yahoo.com>
2015-04-12 15:22:28 -0700

    remove pw button code

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -4,9 +4,4 @@ $(function(){
     $("#flash").remove();
   },5000);
 
-  $("#update-password-btn").click(function() {
-    $("#view-password-fields").toggle();
-    $(this).toggle();  
-  });
-
 });
