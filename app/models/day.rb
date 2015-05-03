bbf00c2e813e2c0717c6c20fac903e7b065778b8
Randy Leighton <randyleighton@yahoo.com>
2015-05-02 17:36:19 -0700

    methods in day for calculate

--- app/models/day.rb
+++ app/models/day.rb
@@ -3,4 +3,15 @@ class Day < ActiveRecord::Base
   belongs_to :timecard
   belongs_to :user
 
+  def total_lunch
+    (lunch_end - lunch_start) / 3600 
+  end
+
+  def total_hours
+    (stop_time - start_time) / 3600
+  end
+
+  def total_day
+    total_hours - total_lunch
+  end
 end
