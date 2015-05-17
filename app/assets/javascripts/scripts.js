2af84f67550c32d1e076bfcc6edab38eb0454c38
Randy Leighton <randyleighton@yahoo.com>
2015-05-17 10:55:15 -0700

    update timecard show for improved open close icons on buttons and layout slight change

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -5,14 +5,17 @@ $(document).ready(function(){
   },5000);
 
   $('#add-workday-btn').click(function(){
+    $('#add-workday-icon-change').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
     $('#add-workday').slideToggle();
   }); 
 
   $("#view-workdays-btn").click(function() {
+    $('#view-workday-icon-change').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
     $('#view-workdays').slideToggle();
   });
 
   $("#view-timecard-summary-btn").click(function() {
+    $('#view-timecard-summary-icon-change').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
     $("#view-timecard-summary").slideToggle();
   });
 
