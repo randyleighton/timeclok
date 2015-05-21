70efc2d2bb6e70b8a6ab7c2c2a34c73d8df65bab
Randy Leighton <randyleighton@yahoo.com>
2015-05-20 21:54:44 -0700

    fixed slidetoggle and timecard show page

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -5,18 +5,18 @@ $(document).ready(function(){
   },5000);
 
   $('#add-workday-btn').click(function(){
-    $('#add-workday-icon-change').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
-    $('#add-workday').toggle();
+    $('#add-workday-icon-change').next().toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+    $('#add-workday').next().toggle();
   }); 
 
   $("#view-workdays-btn").click(function() {
-    $('#view-workday-icon-change').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
-    $('#view-workdays').toggle();
+    $('#view-workday-icon-change').next().toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+    $('#view-workdays').next().toggle();
   });
 
   $("#view-timecard-summary-btn").click(function() {
-    $('#view-timecard-summary-icon-change').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
-    $("#view-timecard-summary").toggle();
+    $('#view-timecard-summary-icon-change').next().toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+    $("#view-timecard-summary").next().toggle();
   });
 
 });
